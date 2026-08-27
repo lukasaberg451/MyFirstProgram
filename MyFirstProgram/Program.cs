92382a5106c762c7f376bf90d6fab9127fc26c54
lukas <lukas@LUKAS>
2026-08-27 18:52:46 +0200

    Add project files.

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyFirstProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
