d82750b66a34c3b61fb512cd38339a21889f1d2f
lukas <lukas@LUKAS>
2026-09-03 16:44:23 +0200

    Final version

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -1,13 +1,14 @@
-﻿namespace MyFirstProgram
+﻿// Lukas Åberg, BUV26
+namespace MyFirstProgram
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int nummer = 6;
+            int number = 6;
 
-            // IF/ELSE to check if the number is greater than 10
-            if (nummer > 10)
+            // IF/ELSE to check if the number is greater than 10.
+            if (number > 10)
             {
                 Console.WriteLine("Talet är stort!");
             }
@@ -16,9 +17,16 @@
                 Console.WriteLine("Oj. Lågt tal!");
             }
 
+            // Ask for the users name and write it back to them.
             Console.WriteLine("Vad heter du?");
-            string namn = Console.ReadLine();
-            Console.WriteLine("Hej, " + namn + "!");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hej {name}!");
+
+            // Loop to write numbers from 0 to the variable value on line 7.
+            for (int i = 0; i <= number; i++)
+            {
+                Console.WriteLine(i);
+            }
 
         }
     }
